0adcc99d158bcc618966bb3f40b2075cb6536697
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-06-30 12:33:15 +0530

    feat: integrated Inngest for background job processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query";
 
 export default function Page() {
   const trpc = useTRPC();
-  trpc.hello.queryKey({ text: 123 })
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
-    <div>
-      Hello world
+    <div className="p-6 max-w-4xl">
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({msg: "Sid"})}>
+        Invoke Background job
+      </Button>
     </div>
   )
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
   hello: baseProcedure
     .input(
@@ -12,6 +13,16 @@ export const appRouter = createTRPCRouter({
         greeting: `hello ${opts.input.text}`,
       };
     }),
+  invoke: baseProcedure.input(z.object({ msg: z.string() }))
+    .mutation(async ({ input }) => {
+      const data = await inngest.send({
+        name: 'test/hello.world',
+        data: {
+          email: input.msg
+        }
+      })
+      return { ok: 'success', msg: data.ids}
+    })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
